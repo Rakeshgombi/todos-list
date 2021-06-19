2deb2121e892eda027ae566ed5dde266045313e0
Rakeshgombi <>
2021-06-19 17:58:09 +0000

    Updates

---
+++ precache-manifest.d9c475d39f7406ef7257fd59269ec437.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a6ea103d5c49256c0eb7daaa6927dfb",
+    "url": "/todos-list/index.html"
+  },
+  {
+    "revision": "e2fdfe91a88cdff2df79",
+    "url": "/todos-list/static/css/main.7f904051.chunk.css"
+  },
+  {
+    "revision": "6eafc991b3bf1dd796b0",
+    "url": "/todos-list/static/js/2.c0cc8fc3.chunk.js"
+  },
+  {
+    "revision": "e2fdfe91a88cdff2df79",
+    "url": "/todos-list/static/js/main.6c8821b6.chunk.js"
+  },
+  {
+    "revision": "a69c6803fe2cd89fa3bc",
+    "url": "/todos-list/static/js/runtime-main.30092cba.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/todo-list/precache-manifest.ccba9c3771b01ce344633f6bfa2a82f5.js"
+  "/todos-list/precache-manifest.d9c475d39f7406ef7257fd59269ec437.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/todo-list/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/todos-list/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
