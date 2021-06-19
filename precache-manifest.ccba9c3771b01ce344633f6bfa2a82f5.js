6264314934ba6811537f5d138052ef032bad50ea
Rakeshgombi <>
2021-06-19 17:43:16 +0000

    Updates

---
+++ precache-manifest.ccba9c3771b01ce344633f6bfa2a82f5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6675fe2d6387ab40ca5b8030a3022023",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "747b2479769c7c8f46f2",
+    "url": "/todo-list/static/css/main.7f904051.chunk.css"
+  },
+  {
+    "revision": "6eafc991b3bf1dd796b0",
+    "url": "/todo-list/static/js/2.c0cc8fc3.chunk.js"
+  },
+  {
+    "revision": "747b2479769c7c8f46f2",
+    "url": "/todo-list/static/js/main.11b2a635.chunk.js"
+  },
+  {
+    "revision": "f93c6fa581d9226145eb",
+    "url": "/todo-list/static/js/runtime-main.434366f4.js"
+  }
+]);
